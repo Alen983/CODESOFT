5e13ae0e42d17d85c919244cb286622a242e2d9d
AV <alenv983@gmail.com>
2024-07-15 17:41:58 +0530

    Task 3 Password gen

---
+++ Password Generator.py
@@ -0,0 +1,14 @@
+import random
+
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = uppercase.lower()
+symbols = "@$%^&*(())_+-.?,';:: "
+numer = "1234567890"
+total = uppercase+lowercase+symbols+numer
+
+size = int(input("Enter the length of your password"))
+
+
+password = "".join(random.sample(total, size))
+print("Your Password is as follows:")
+print(password)
