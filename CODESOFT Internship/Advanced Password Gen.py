b17fea8dbdd7470c65003e03250c648d1999c609
Alen983 <81340664+Alen983@users.noreply.github.com>
2024-08-07 20:54:07 +0530

    Self Task

--- CODESOFT Internship/Advanced Password Gen.py
+++ CODESOFT Internship/Advanced Password Gen.py
@@ -5,4 +5,4 @@ size = int(input("Enter the size of the password"))
  
 total = string.ascii_letters + string.digits + string.punctuation
 password = "".join(random.sample(total,size))
-print(password)
+print(password)  
